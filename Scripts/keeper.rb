53e6daa09a3e3b1743e0f390faacb4f050ffa565
ErrorCode191853 <khoinguyenhuu20@gmail.com>
2026-03-14 04:04:46 +0100

    Features: Automatically sorts files by file extension.

---
+++ Scripts/keeper.rb
@@ -0,0 +1,50 @@
+require 'fileutils'
+
+class GemsKeeper
+  # Define 'boxes' for files
+  # A Hash (similar to Dictionary) to store rules and quick lookup.
+  RULES = {
+    '.txt'  => 'Documents',
+    '.rb'   => 'Scripts',
+    '.jpg'  => 'Images',
+    '.png'  => 'Images',
+    '.pdf'  => 'Books'
+  }
+
+  def initialize(path)
+    @path = path
+  end
+
+  def organize
+    puts "--- GemsKeeper is sorting... ---"
+
+    files = Dir.children(@path)
+
+    files.each do |file_name|
+      # Acquiring the full path of files
+      full_path = File.join(@path, file_name)
+
+      # Ignore it if it is a directory or the main file of the project.
+      next if File.directory?(full_path) || file_name == 'guard.rb'
+
+      extension = File.extname(file_name).downcase
+
+      # Check the extension of the file. If it is not in the rules, it will be ignored.
+      folder_name = RULES[extension] || 'Others'
+
+      # Create folders if not exist
+      dest_folder = File.join(@path, folder_name)
+      FileUtils.mkdir_p(dest_folder)
+
+      # Move files to boxes
+      FileUtils.mv(full_path, File.join(dest_folder, file_name))
+      puts "✨ moved [#{file_name}] into 'box' [#{folder_name}]"
+    end
+
+    puts "--- finished! Directory is 'cleaned'. ---"
+  end
+end
+
+# Carries out the task.
+keeper = GemsKeeper.new(".")
+keeper.organize
